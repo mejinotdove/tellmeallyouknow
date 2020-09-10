5e10c31bff72c7e6ecc4a1a553d97eb280b68cb2
mejinotdove <mejinotdove@users.noreply.github.com>
2020-09-10 20:45:45 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,30 +1,75 @@
 #!/bin/sh
 
-# Download and install tls-shunt-proxy
-mkdir /tmp/tls-shunt-proxy
-curl -L -H "Cache-Control: no-cache" -o /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip https://github.com/liberal-boy/tls-shunt-proxy/releases/latest/download/tls-shunt-proxy-linux-amd64.zip
-unzip /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip -d /tmp/tls-shunt-proxy
-install -m 755 /tmp/tls-shunt-proxy/tls-shunt-proxy /usr/local/bin/tls-shunt-proxy
+cat << EOF > /etc/nginx/nginx.conf
+server { 
+ listen $PORT;
 
-# Remove temporary directory
-rm -rf /tmp/tls-shunt-proxy
-
-# tls-shunt-proxy new configuration
-install -d /usr/local/etc/tls-shunt-proxy
-cat << EOF > /usr/local/etc/tls-shunt-proxy/config.yaml
-listen: :$PORT
-inboundbuffersize: 4
-outboundbuffersize: 32
-vhosts:
-  - name: $HOST
-    http:
-      paths:
-        - path: $VMESS_WS_PATH
-          handler: proxyPass
-          args: 127.0.0.1:12345
-        - path: $SOCKS_WS_PASS
-          handler: proxyPass
-          args: 127.0.0.1:12346
+ location / {
+   # This would be the directory where your React app's static files are stored at
+   root /usr/share/nginx/html;
+   try_files $uri /index.html;
+ }
+ 
+ location $VMESS_WS_PATH {
+		proxy_pass http://localhost:12345;
+		proxy_redirect off;
+		proxy_http_version 1.1;
+		proxy_set_header Upgrade $http_upgrade;
+		proxy_set_header Connection "upgrade";
+		proxy_set_header Host $http_host;
+ }
+}
 EOF
 
-/usr/local/bin/tls-shunt-proxy -config /usr/local/etc/tls-shunt-proxy/config.yaml
+nohup nginx >/dev/null 2>&1 &
+
+# Download and install V2Ray
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+    "log":{
+        "access": null,
+        "error":  null,
+        "loglevel": "info"
+    },
+    "inbounds": [
+        {
+            "port": 12345,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "security": "none",
+                        "alterId": 0
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "$VMESS_WS_PATH"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run V2Ray
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
