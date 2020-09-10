c5dfbcbed2afff9407fa6137a6c72528e49a9711
mejinotdove <mejinotdove@users.noreply.github.com>
2020-09-10 20:05:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Download and install tls-shunt-proxy
-mkdir /tmp/tls-shunt-proxy
-curl -L -H "Cache-Control: no-cache" -o /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip https://github.com/liberal-boy/tls-shunt-proxy/releases/latest/download/tls-shunt-proxy-linux-amd64.zip
-unzip /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip -d /tmp/tls-shunt-proxy
-install -m 755 /tmp/tls-shunt-proxy/tls-shunt-proxy /usr/local/bin/tls-shunt-proxy
-
-# Remove temporary directory
-rm -rf /tmp/tls-shunt-proxy
-
 # tls-shunt-proxy new configuration
 install -d /usr/local/etc/tls-shunt-proxy
 cat << EOF > /usr/local/etc/tls-shunt-proxy/config.yaml
@@ -28,4 +19,4 @@ vhosts:
 EOF
 
 # Run proxy
-/usr/local/bin/tls-shunt-proxy -config /usr/local/etc/tls-shunt-proxy/config.yaml
+/usr/local/bin/proxy -config /usr/local/etc/tls-shunt-proxy/config.yaml
