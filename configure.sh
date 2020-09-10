b3b4c9890951646687e0d6e098a786b890cad3f1
mejinotdove <mejinotdove@users.noreply.github.com>
2020-09-10 17:55:26 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,7 +14,7 @@ rm -rf /tmp/v2ray
 mkdir /tmp/tls-shunt-proxy
 curl -L -H "Cache-Control: no-cache" -o /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip https://github.com/liberal-boy/tls-shunt-proxy/releases/latest/download/tls-shunt-proxy-linux-amd64.zip
 unzip /tmp/tls-shunt-proxy/tls-shunt-proxy-linux-amd64.zip -d /tmp/tls-shunt-proxy
-install -m 755 /tmp/tls-shunt-proxy/tls-shunt-proxy /usr/local/bin/v2ray/tls-shunt-proxy
+install -m 755 /tmp/tls-shunt-proxy/tls-shunt-proxy /usr/local/bin/tls-shunt-proxy
 
 # Remove temporary directory
 rm -rf /tmp/tls-shunt-proxy
